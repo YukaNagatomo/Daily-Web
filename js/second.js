31baddf0d39aaea4afd1f456b3bb68394760a092
YukaNagatomo <chatsumi.yuchan@gmail.com>
2019-04-28 09:54:14 +0900

    update name

--- js/second.js
+++ js/second.js
@@ -1,39 +1,44 @@
 $(function () {
 
-//    $(window).resize(function () {
-//        location.reload();
-//    })
+  //    $(window).resize(function () {
+  //        location.reload();
+  //    })
 
-    //    fullpage.js
-    $('#fullpage').fullpage({
-        navigation: true,
-        resize: true
-    });
-    $(window).on('load resize', function () {
-        responsive();
-    });
+  //    fullpage.js
+  $('#fullpage').fullpage({
+    navigation: true,
+    resize: true
+  });
+  $(window).on('load resize', function () {
+    responsive();
+  });
 
-    $(document).ready(function responsive() {
-        var w = $(window).width();
-        if (w <= 768) {
-            $.fn.fullpage.setResponsive(true);
-        } else {
-            $.fn.fullpage.setResponsive(false);
-        }
-    });
+  $(document).ready(function responsive() {
+    var w = $(window).width();
+    if (w <= 768) {
+      $.fn.fullpage.setResponsive(true);
+    } else {
+      $.fn.fullpage.setResponsive(false);
+    }
+  });
 
-    //    bxslider.js
-    $('.slider').bxSlider({
-        auto: true,
-        mode: 'fade',
-        controls: false,
-        pager: false
-    });
+  //    bxslider.js
+  $('.slider').bxSlider({
+    auto: true,
+    mode: 'fade',
+    controls: false,
+    pager: false
+  });
 
-    //    inview
-    $('.slide1').on('inview', function () {
-        //    jquery.ShuffleLetters.js
-        $('.shuffle').shuffleLetters();
-    });
+  // bgswichwer
+  $(".bg-slider").bgswitcher({
+    images: ["./images/pink1.jpeg", "./images/pink2.jpeg", "./images/pink3.jpeg"],
+  });
+
+  //    inview
+  $('.slide1').on('inview', function () {
+    //    jquery.ShuffleLetters.js
+    $('.shuffle').shuffleLetters();
+  });
 
 });
